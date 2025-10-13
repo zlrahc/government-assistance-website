b569ac5d9227d47bd0aca889a29b430d8f84a1e3
Charlz <charlzsrsly@gmail.com>
2025-10-13 20:46:36 +0800

    feat(offices): -

---
+++ server/data/office-database.js
@@ -0,0 +1,83 @@
+const officesByRegion = {
+  "NCR": [
+    {
+      id: 1,
+      name: "Manila City Hall",
+      address: "Padre Burgos Ave, Ermita, Manila, 1000 Metro Manila",
+      contact: "(02) 8527 0972",
+      lat: 14.589793,
+      lon: 120.981617,
+      hours: { open: "07:00", close: "17:00" },
+    },
+    {
+      id: 2,
+      name: "Quezon City Hall",
+      address: "High Rise Building, Elliptical Road, Mayaman, Diliman, Quezon City, 1101 Kalakhang Maynila",
+      contact: "(02) 8988 4242",
+      lat: 14.6465,
+      lon: 121.0501,
+      hours: { open: "08:00", close: "17:00" },
+    },
+    {
+      id: 3,
+      name: "Pasig City Hall",
+      address: "H35J+RFW, Caruncho Ave, Barangay San Nicolas, Pasig, 1600 Metro Manila",
+      contact: "(02) 8643 1111",
+      lat: 14.559725528864913,
+      lon: 121.08116451037284,
+      hours: { open: "08:00", close: "17:00" },
+    },
+
+  ],
+
+  "Region IV-A": [
+    {
+      id: 4,
+      name: "Old Dasmariñas City Hall",
+      address: "Zone 4, Don Placido Campos Avenue, Dasmariñas, 4114 Cavite",
+      contact: "(046) 481 4600",
+      lat: 14.326825072190655,
+      lon: 120.93684474530212,
+      hours: { open: "08:00", close: "17:00" },
+    },
+    {
+      id: 2,
+      name: "Cavite Provincial Capitol",
+      address: "Trece Martires City, Cavite",
+      contact: "(046) 419-2311",
+      lat: 14.2833, lon: 120.8667,
+      hours: { open: "08:00", close: "17:00" },
+    },
+    {
+      id: 3,
+      name: "Tagaytay City Hall",
+      address: "Tagaytay City, Cavite",
+      contact: "(046) 413-2122",
+      lat: 14.0977, lon: 120.9320,
+      hours: { open: "09:00", close: "16:00" },
+    }
+  ],
+
+  "Region I": [
+    {
+      id: 1,
+      name: "Ilocos Norte Provincial Capitol",
+      address: "J. P. Rizal St, Laoag City, 2900 Ilocos Norte",
+      contact: "(077) 772 1211",
+      lat: 18.196419655901646,
+      lon: 120.59368316626137,
+      hours: { open: "08:00", close: "17:00" },
+    },
+    {
+      id: 2,
+      name: "Ilocos Sur Provincial Capitol",
+      address: "H9GP+8JM, Quezon Ave, Vigan City, Ilocos Sur",
+      contact: "(077) 722 2740",
+      lat: 17.57597306120365,
+      lon: 120.38660663982228,
+      hours: { open: "08:00", close: "17:00" },
+    },
+  ]
+};
+
+export default officesByRegion;
